79567728b90f3b3cdb30e0193f26257fb7845778
blendonluta <blendonluta@gmail.com>
2023-05-30 08:05:41 +0200

    add user model

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+
 
 }
