eae1fb8db6439419b04bc7af524efafccc7f3502
blendonluta <blendonluta@gmail.com>
2023-06-04 19:05:35 +0200

    add users details

--- src/app/users/users-details/users-details.component.ts
+++ src/app/users/users-details/users-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'users-details',
@@ -8,6 +10,28 @@ import { User } from 'src/app/models/user.model';
 })
 export class UsersDetailsComponent {
 
-  @Input() user: User | undefined = undefined;
+  user: User | undefined
+
+  constructor(private route: ActivatedRoute, private userService: UserService) { 
+  }
+
+  ngOnInit(): void {
+
+    let id: string = this.route.snapshot.paramMap.get('id') ?? ''
+
+    this.getUser(id)
+    
+
+  }
+
+  getUser(id: string) {
+
+    this.userService.getUser(id).subscribe((data: User) => {
+      this.user = data;
+
+      
+    })
+
+  }
 
 }
--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/users.service';
+import { WebSocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'users-list',
@@ -11,21 +13,28 @@ export class UsersListComponent {
 
   users: User[] = []
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private webSocketService: WebSocketService, private router: Router) {
 
     this.getUsers()
   }
 
+
+
   getUsers() {
 
+    this.webSocketService.connect().subscribe()
+
     this.userService.getUsers().subscribe((data) => {
+      console.log(data);
+      
 
       (data as []).forEach((u :any) => this.users.push(new User(u.firstName, u.lastName, u.email, u.password, u.teamName)))
-
-
     })
+  }
 
-    
+  onRowClick(index: number) {
+
+    this.router.navigateByUrl('users/' + (index + 1))
 
   }
   
