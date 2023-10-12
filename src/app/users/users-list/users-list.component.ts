4e4a6094004dfc90115daebe1ac6e94e7ba9f65e
blendonl <blendonluta@gmail.com>
2023-10-12 05:39:58 +0200

    delete websocket

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/users.service';
-import { WebSocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'users-list',
@@ -13,7 +12,7 @@ export class UsersListComponent {
 
   users: User[] = []
 
-  constructor(private userService: UserService, private webSocketService: WebSocketService, private router: Router) {
+  constructor(private userService: UserService, private router: Router) {
 
     this.getUsers()
   }
@@ -21,13 +20,7 @@ export class UsersListComponent {
 
 
   getUsers() {
-
-    this.webSocketService.connect().subscribe()
-
     this.userService.getUsers().subscribe((data) => {
-    console.log(data);
-    
-
       (data as []).forEach((u :any) => this.users.push(new User(u.firstName, u.lastName, u.email, u.password, u.teamName)))
     })
   }
