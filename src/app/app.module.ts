d5aded84578d4561aec50fa378bac189555ee141
blendonl <blendonluta@gmail.com>
2023-10-12 05:45:15 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     UsersModule,
-        HttpClientModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
